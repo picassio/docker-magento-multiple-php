01c88af75015b5c097a486d5b497dc87d28c9ae7
TuanLH SOSC Laptop <tuanlh@smartosc.com>
2023-02-17 17:14:44 +0700

    Correct command

--- build/php73/start.sh
+++ build/php73/start.sh
@@ -1,2 +1,16 @@
 #! /bin/bash
+set -e 
+
+RUN sed -i -e "s/REPLACE_WITH_REAL_KEY/${NEW_RELIC_LICENSE_KEY}/" \
+    -e "s/newrelic.appname[[:space:]]=[[:space:]].*/newrelic.appname=\"${NEW_RELIC_APPNAME}\"/" \
+    -e '$anewrelic.distributed_tracing_enabled=true' \
+    -e '$anewrelic.daemon.address="newrelic:31339"' \
+    /etc/php/7.3/fpm/conf.d/newrelic.ini
+
+RUN sed -i -e "s/REPLACE_WITH_REAL_KEY/${NEW_RELIC_LICENSE_KEY}/" \
+    -e "s/newrelic.appname[[:space:]]=[[:space:]].*/newrelic.appname=\"${NEW_RELIC_APPNAME}\"/" \
+    -e '$anewrelic.distributed_tracing_enabled=true' \
+    -e '$anewrelic.daemon.address="newrelic:31339"' \
+    /etc/php/7.3/cli/conf.d/newrelic.ini
+    
 /usr/sbin/php-fpm7.3 -F -O --fpm-config /etc/php/7.3/fpm/php-fpm.conf
